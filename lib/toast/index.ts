374e88ccdafab7bc750395e16c01958ad7e10364
Flores Rodriguez, Max Alejandro - Externo <MaxAlejandro.FloresRodriguez@fuller.com.mx>
2026-02-17 15:55:39 -0600

    feat: export NgbToastOptions type from NgbToast model

--- lib/toast/index.ts
+++ lib/toast/index.ts
@@ -1,2 +1,3 @@
 export { NgbToastModule } from "@/toast/ngb-toast.module"
 export { NgbToastConfig } from "@/toast/ngb-toast-config.service"
+export type { NgbToastOptions } from "@/toast/ngb-toast.model"
